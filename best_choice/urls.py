07a9584d7bc8ffc26ab8a35044f7926ed3ecebce
Phạm Trung Đức <ducphamtrung.devw@gmail.com>
2023-09-05 14:09:12 +0700

    #2: Store page

--- best_choice/urls.py
+++ best_choice/urls.py
@@ -25,4 +25,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('store/', include('store.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,1 +1,17 @@
+from django.shortcuts import get_object_or_404, render
 
+from category.models import Category
+from store.models import Product
+
+
+def store(request, category_slug=None):
+    if category_slug is not None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.all().filter(category=categories, is_available=True)
+    else:
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+
+    context = {
+        'products': products
+    }
+    return render(request, 'store/store.html', context=context)
