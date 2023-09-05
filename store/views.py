c96cedbd94303d4b12dc36739de0b4db6d8ab364
Phạm Trung Đức <ducphamtrung.devw@gmail.com>
2023-09-05 20:49:50 +0700

    #6: Search products

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 from category.models import Category
 from store.models import Product
@@ -32,3 +33,14 @@ def product_detail(request, category_slug, product_slug=None):
 
     return render(request, 'store/product_detail.html', context=context)
 
+def search(request):
+    if 'q' in request.GET:
+        q = request.GET.get('q')
+        products = Product.objects.order_by('-created_date').filter(Q(product_name__icontains=q) | Q(description__icontains=q))
+        product_count = products.count()
+    context = {
+        'products': products,
+        'q': q,
+        'product_count': product_count
+    }
+    return render(request, 'store/store.html', context=context)
