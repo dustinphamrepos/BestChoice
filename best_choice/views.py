11f4458eef0e70edc8ebd1a164bf2bef4707f36c
Phạm Trung Đức <ducphamtrung.devw@gmail.com>
2023-09-05 10:11:15 +0700

    #1: Start project, create models, staticfiles, config admin-site and homepage

---
+++ best_choice/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from store.models import Product
+
+
+def home(request):
+  products = Product.objects.all().filter(is_available=True)
+  context = {
+    'product': products,
+  }
+  return render(request, 'home.html', context=context)
