4af2938bc2db08dd1812824928f4c5b69d327f5d
Phạm Trung Đức <ducphamtrung.devw@gmail.com>
2023-09-08 17:09:08 +0700

    #11: Register

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from .models import Account
+from .forms import RegistrationForm
 
 # Create your views here.
+def register(request):
+  if request.method == 'POST':
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+      first_name = form.cleaned_data['first_name']
+      last_name = form.cleaned_data['last_name']
+      email = form.cleaned_data['email']
+      phone_number = form.cleaned_data['phone_number']
+      password = form.cleaned_data['password']
+      username = email.split('@')[0]
+
+      user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+      user.phone_number = phone_number
+      user.save()
+      return redirect('register')
+    else:
+      messages.error(request=request, message="Register failed!")
+  else:
+    form = RegistrationForm()
+  context = {
+    'form': form,
+  }
+  return render(request, 'accounts/register.html', context=context)
