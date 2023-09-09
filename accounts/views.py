416c8f707d3b1a3475016e8ae3dad6445e8de755
Phạm Trung Đức <ducphamtrung.devw@gmail.com>
2023-09-09 09:07:59 +0700

    #14: Login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 
 from django.shortcuts import redirect, render
-from django.contrib import messages
+from django.contrib import messages, auth
+from carts.models import Cart, CartItem
+from carts.views import _cart_id
 from .models import Account
 from .forms import RegistrationForm
 
@@ -29,3 +31,52 @@ def register(request):
     'form': form,
   }
   return render(request, 'accounts/register.html', context=context)
+
+def login(request):
+  if request.method == 'POST':
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    user = auth.authenticate(email=email, password=password)
+    if user is not None:
+      try:
+        cart = Cart.objects.get(cart_id=_cart_id(request=request))
+        cart_items = CartItem.objects.filter(cart=cart)
+        if cart_items.exists():
+          product_variation = []
+          for cart_item in cart_items:
+            variations = cart_item.variations.all()
+            product_variation.append(list(variations))
+
+          existing_cart_items = CartItem.objects.filter(user=user)
+          existing_variation_list = [list(item.variations.all()) for item in existing_cart_items]
+          id = [item.id for item in existing_cart_items]
+
+          for product in product_variation:
+            if product in existing_variation_list:
+              index = existing_variation_list.index(product)
+              item_id = id[index]
+              item = CartItem.objects.get(id=item_id)
+              item.quantity += 1
+              item.user = user
+              item.save()
+            else:
+              cart_items = CartItem.objects.filter(cart=cart)
+              for item in cart_items:
+                item.user = user
+                item.save()
+
+      except Exception:
+        pass
+      auth.login(request=request, user=user)
+      messages.success(request=request, message='Login succeed!')
+      return redirect('home')
+    else:
+      messages.error(request=request, message="Login failed!")
+  
+  context = {
+    'email': email if 'email' in locals() else '',
+    'password': password if 'password' in locals() else ''
+  }
+
+  return render(request, 'accounts/login.html', context=context)
+
